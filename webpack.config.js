960d9912cbdb5987b157d950533e29bc2a88dd36
Wai San Gu <waisangu@gmail.com>
2023-06-20 16:47:16 -0400

    delete app.jsx renamed to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: './client/index.jsx',
+  entry: './client/index.js',
   output: {
     path: path.resolve(__dirname, './build'),
     publicPath: '/',
