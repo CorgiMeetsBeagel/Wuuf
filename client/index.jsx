b2d54101753a5b780b2f0ee817eb49555712df28
Wai San Gu <waisangu@gmail.com>
2023-06-20 16:15:35 -0400

    renamed js to jsx

---
+++ client/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+import MatchPage from './pages/MatchPage.jsx';
+import SwipePage from './pages/SwipePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage />,
+  },
+  {
+    path: '/matches',
+    element: <MatchPage />,
+  },
+  {
+    path: '/swipe',
+    element: <SwipePage />,
+  }
+]);
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
